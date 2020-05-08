c7fa6fb0ccb2cea097a56a75cbd204458c8b092b
Inna <ikunygina@yandex.ru>
2020-05-08 14:18:22 +0300

    create server

---
+++ db.py
@@ -0,0 +1,30 @@
+import aiopg.sa
+
+from sqlalchemy import (
+    MetaData, Table, Column,
+    Integer, String,
+)
+
+meta = MetaData()
+
+user = Table(
+    'user', meta,
+    Column('id', Integer, primary_key=True),
+    Column('username', String(200), nullable=False)
+)
+
+
+async def init_pg(app):
+    engine = await aiopg.sa.create_engine(
+        database='postgres',
+        user='postgres',
+        password='docker',
+        host='localhost',
+        port='5432',
+    )
+    app['db'] = engine
+
+
+async def close_pg(app):
+    app['db'].close()
+    await app['db'].wait_closed()
--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('Hello')
+from aiohttp import web
+from db import close_pg, init_pg
+import db
+
+
+async def handle(request):
+    async with request.app['db'].acquire() as conn:
+        cursor = await conn.execute(db.user.select())
+        records = await cursor.fetchall()
+        users = [dict(u) for u in records]
+        return web.Response(text=str(users))
+
+app = web.Application()
+app.on_startup.append(init_pg)
+app.on_cleanup.append(close_pg)
+app.add_routes([web.get('/', handle),
+                web.get('/{name}', handle)])
+
+if __name__ == '__main__':
+    web.run_app(app)
